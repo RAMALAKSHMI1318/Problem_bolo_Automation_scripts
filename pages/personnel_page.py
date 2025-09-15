a790d8a92a607f7657300b3c07c27a9becb72654
Rani <kuruvarani27@gmail.com>
2025-09-15 11:23:11 +0530

    personnel module completed

--- pages/personnel_page.py
+++ pages/personnel_page.py
@@ -32,8 +32,8 @@ class PersonnelPage(BasePage):
         self.option_hydcity = page.get_by_role("option", name="HyderabadCity")
 
         # --- Step 3: Personnel details ---
-        self.input_first_name = page.get_by_role("textbox", name="Enter Name").nth(0)
-        self.input_last_name = page.get_by_role("textbox", name="Enter Name").nth(1)
+        self.input_first_name = page.get_by_placeholder("Enter First Name")
+        self.input_last_name = page.get_by_placeholder("Enter Last Name")
         self.input_phone = page.locator("input[type='tel']")
         self.input_email = page.get_by_role("textbox", name="Enter Email")
         self.input_empid = page.get_by_role("textbox", name="Enter Emp-id")
@@ -42,6 +42,16 @@ class PersonnelPage(BasePage):
         # --- Navigation buttons ---
         self.btn_next = page.get_by_role("button", name="Next")
         self.btn_done = page.get_by_role("button", name="Done")
+        self.profile_admin = page.get_by_text("ProfileAdministrator")
+        self.cmb_institution = page.get_by_role("combobox")
+
+        self.input_search_personnel = page.get_by_role("textbox", name="Search Personnel")
+        self.btn_edit_personnel_icon = page.get_by_role("button", name="primaryEditIcon")
+        self.txt_edit_personnel_header = page.get_by_text("Edit PersonnelAuto Saved To")
+        self.input_last_name_edit = page.get_by_role("textbox").nth(1)   # based on your steps
+        self.btn_edit_personnel_submit = page.get_by_role("button", name="Edit Personnel")
+        self.btn_view_personnel = page.get_by_role("button", name="primaryEyeIcon")
+        self.btn_close_view = page.get_by_role("button", name="Close")
 
     # ---------------- ACTION METHODS ----------------
     def navigate_to_personnel(self):
@@ -111,3 +121,38 @@ class PersonnelPage(BasePage):
         self.cmb_area.click()
         self.option_hydcity.click()
 
+    def assign_institution_admin(self, institution_name: str):
+       
+        self.btn_get_started.click()
+        self.btn_personnel.click()
+        self.btn_add_personnel.click()
+
+        self.dropdown_org_type.click()
+        self.option_administrator.click()
+        self.profile_admin.click()
+
+        self.btn_next.click()
+
+        self.cmb_institution.click()
+        self.page.get_by_role("option", name=institution_name).click()
+
+    def edit_personnel(self, search_name: str, updated_last_name: str):
+    # Step 1: Navigate to Personnel
+        self.btn_get_started.click()
+        self.btn_personnel.click()
+        self.input_search_personnel.click()
+        self.input_search_personnel.fill(search_name)
+        self.btn_edit_personnel_icon.click()
+        self.txt_edit_personnel_header.wait_for()
+        self.input_last_name_edit.click()
+        self.input_last_name_edit.fill(updated_last_name)
+        self.btn_edit_personnel_submit.click()
+
+    def view_personnel(self, search_name: str):
+        self.btn_get_started.click()
+        self.btn_personnel.click()
+        self.input_search_personnel.click()
+        self.input_search_personnel.fill(search_name)
+        self.btn_view_personnel.click()
+        self.btn_close_view.click()
+       
